0cbb5bb20eed8d99c2fe52b127529f5c076de455
Aditya <adityawatekar835@gmail.com>
2025-11-24 16:37:51 +0530

    admin pannel url add

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://localhost:4173","http://localhost:5174", "https://tarstechnologies.vercel.app" ],
+    origin: ["http://localhost:5173","http://localhost:4173","http://localhost:5174", "https://tarstechnologies.vercel.app", "https://tars-technologies-admin.vercel.app"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
